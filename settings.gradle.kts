dd8112f8716501bff3b6c197a856333f77a4b883
Mark Zorikhin <hnau256@gmail.com>
2026-07-06 21:12:15 +0300

    init

---
+++ settings.gradle.kts
@@ -0,0 +1,14 @@
+rootProject.name = "LLMChat"
+
+pluginManagement {
+    repositories {
+        mavenLocal()
+        gradlePluginPortal()
+        google()
+        mavenCentral()
+    }
+}
+
+plugins {
+    id("org.hnau.plugin.settings") version "1.23.5"
+}
